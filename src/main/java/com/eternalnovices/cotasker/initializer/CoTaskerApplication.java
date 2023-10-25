28aea7363c91022766a17a92af3be2fb710c5e11
Andres Ochoa <andresochoaflorez0@gmail.com>
2023-10-25 05:18:17 -0500

    se inicializa el repositorio con la estructura de las carpetas definidas

---
+++ src/main/java/com/eternalnovices/cotasker/initializer/CoTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.eternalnovices.cotasker.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoTaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoTaskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eternalnovices/cotasker/initializer/CoTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternalnovices.cotasker.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoTaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
